7fe4e02166532eb3eef9b28b9171f1c008642347
Akash <akashsuper2000@gmail.com>
2021-03-03 22:47:44 +0530

    job update

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-81a4765f7098a95f0be0.js"
+    "url": "webpack-runtime-9b07a52ef52940e79418.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "896a0f73e512093d017290692370e4e1"
+    "revision": "194926b6acf159a30c270308de040b79"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c51f55ab17ee4b16f9fe78dbb517b1b9"
+    "revision": "cf4c5a175df9347e22959ec598c61964"
   },
   {
     "url": "manifest.webmanifest",
